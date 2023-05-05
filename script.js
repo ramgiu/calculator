55a2289bb43f559571fcc9c228fd915ca0d4047d
ramgiu <ramiro.giunta@gmail.com>
2023-05-05 20:47:30 -0300

    Adding styles and trying functions

---
+++ script.js
@@ -0,0 +1,14 @@
+let uno = document.getElementById ('uno')
+let valorUno = uno.innerHTML
+let numeroUno = parseInt(valorUno)
+
+let dos = document.getElementById ('dos')
+let valorDos = dos.innerHTML
+let numeroDos = parseInt(valorDos)
+
+
+
+function sumando () {
+    document.write(numeroUno + numeroDos)
+}
+   
